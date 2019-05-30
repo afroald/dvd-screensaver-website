16fe0f3d2161b781cd147d3f53205ce8f4696379
Roald Hacquebord <roald@zielma.kr>
2019-05-30 18:00:02 +0200

    Add link to repository

--- components/Credits.vue
+++ components/Credits.vue
@@ -1,21 +1,35 @@
 <template>
-  <a
-    :class="$style.link"
-    href="https://roaldhacquebord.nl"
-    target="_blank"
-  >roald hacquebord</a>
+  <div :class="$style.credits">
+    <a
+      :class="$style.link"
+      href="https://github.com/afroald/dvd-screensaver-website"
+      target="_blank"
+    >source</a>
+    —
+    <a
+      :class="$style.link"
+      href="https://roaldhacquebord.nl"
+      target="_blank"
+    >roald hacquebord</a>
+  </div>
 </template>
 
 <style lang="scss" module>
-.link {
-  font-family: Helvetica, sans-serif;
-  color: #fff;
-  opacity: .3;
-  text-decoration: none;
+.credits {
+  opacity: .15;
   transition: opacity .06s ease-in-out;
 
   &:hover {
-    opacity: 1;
+    opacity: .8;
+  }
+}
+
+.link {
+  font-family: Helvetica, sans-serif;
+  color: #fff;
+  text-decoration: none;
+
+  &:hover {
     text-decoration: underline;
   }
 }
